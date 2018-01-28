2a3d76587b2ba4e674b83ece73d542f5626b29e4
jkvim <jkvim@outlook.com>
2018-01-28 23:13:56 +0800

    chore(config): add git cz-cli config

---
+++ cz-config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+
+  types: [
+    {value: 'feat',     name: 'feat:     A new feature'},
+    {value: 'fix',      name: 'fix:      A bug fix'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
+    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
+  ],
+
+  scopes: [
+    {name: 'git'},
+    {name: 'mobile'},
+    {name: 'frontend'},
+    {name: 'bugs'},
+    {name: 'auth'},
+    {name: 'soft-skill'},
+    {name: 'algorithm'}
+  ],
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix']
+}
