30732773099ded55fb2c34f3d2443d69eafde004
jkvim <jkvim@outlook.com>
2019-07-04 22:19:29 +0800

    chore(git): update cz-config

--- cz-config.js
+++ cz-config.js
@@ -1,26 +1,108 @@
-'use strict';
+const map = require("lodash/map");
 
 module.exports = {
-
-  types: [
-    {value: 'feat',     name: 'feat:     A new feature'},
-    {value: 'fix',      name: 'fix:      A bug fix'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
-    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
-  ],
-
-  scopes: [
-    {name: 'git'},
-    {name: 'graphql'},
-    {name: 'mobile'},
-    {name: 'frontend'},
-    {name: 'bugs'},
-    {name: 'auth'},
-    {name: 'soft-skill'},
-    {name: 'algorithm'},
-    {name: 'shell'}
-  ],
-
-  allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix']
+  prompter: prompter,
+  formatCommit: formatCommit
 };
+
+const types = {
+  feat: {
+    description: "A new feature",
+    title: "Features"
+  },
+  fix: {
+    description: "A bug fix",
+    title: "Bug Fixes"
+  },
+  chore: {
+    description: "Other changes that don't modify src or test files",
+    title: "Chores"
+  },
+  docs: {
+    description: "Documentation only changes",
+    title: "Documentation"
+  },
+  style: {
+    description:
+      "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+    title: "Styles"
+  },
+  refactor: {
+    description: "A code change that neither fixes a bug nor adds a feature",
+    title: "Code Refactoring"
+  },
+  perf: {
+    description: "A code change that improves performance",
+    title: "Performance Improvements"
+  },
+  test: {
+    description: "Adding missing tests or correcting existing tests",
+    title: "Tests"
+  },
+  build: {
+    description:
+      "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+    title: "Builds"
+  },
+  ci: {
+    description:
+      "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
+    title: "Continuous Integrations"
+  },
+  revert: {
+    description: "Reverts a previous commit",
+    title: "Reverts"
+  }
+};
+
+function prompter(cz, commit) {
+  cz.prompt([
+    {
+      type: 'list',
+      name: 'type',
+      message: 'select type of change (required):\n',
+      choices: map(types, (type, key) => {
+        return {
+          name: `${key}: ${type.description}`,
+          value: key
+        };
+      })
+    },
+    {
+      type: 'list',
+      name: 'scope',
+      choices: [
+        { name: 'git', value: 'git' },
+        { name: 'frontend', value: "frontend" },
+        { name: 'graphql', value: "graphql"},
+        { name: 'mobile', value: "mobile" },
+        { name: 'bugs', value: "bugs" },
+        { name: 'auth', value: "auth" },
+        { name: 'soft-skill', value: "soft-skill" },
+        { name: 'algorithm', value: "algorithm" },
+        { name: 'shell', value: "shell" },
+        { name: 'config', value: "config" }
+      ],
+      message: 'select scope (require):\n'
+    },
+    {
+      type: 'input',
+      name: 'message',
+      message: 'commit message (required):\n',
+      validate: function(input) {
+        if (!input) {
+          return 'empty commit message';
+        } else {
+          return true;
+        }
+      }
+    }
+  ]).then(answers => {
+    formatCommit(commit, answers);
+  });
+}
+
+function formatCommit(commit, answers) {
+  const { type, scope, message } = answers;
+  commit(`${type}(${scope}): ${message}`);
+}
